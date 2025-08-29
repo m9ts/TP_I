0ebf333d179b398e57d94de22d8c54ad8842eae1
m9ts <mat3us.gois@gmail.com>
2025-08-29 19:35:38 -0300

    Interpolação

--- Loja_TP1_Angular/loja-tp1-angular/src/app/core/header/header.ts
+++ Loja_TP1_Angular/loja-tp1-angular/src/app/core/header/header.ts
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './header.css'
 })
 export class Header {
+  titulo = 'O site dos gigantes!';
+  exibirSobre(nome: string): void {
+    alert(`Olá, meu ${nome}!`); // Interpolação: inserção de valores dinâmicos em strings ou expressões
+  }
 
 }
